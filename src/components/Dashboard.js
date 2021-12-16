47b7af4a6d8951c668ef5ce715f2cdc2fa885d0e
Taylor Noj <taylornoj@gmail.com>
2021-12-16 16:43:57 -0500

    am able to toggle between panel view and focused view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,22 +29,34 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
     const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
       .map(panel => (
         <Panel
           key={panel.id}
-          id={panel.id}
           label={panel.label}
           value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
